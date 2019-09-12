89e6c6eb796bb4fb583cb59262e89040d2fd5db8
ellinorstenhaug <ellinor.stenhaug@cortinate.se>
2019-09-12 10:07:39 +0200

    animations etc

---
+++ assets/js/main.js
@@ -0,0 +1,29 @@
+  $('.carousel').carousel({
+    interval: 5000
+  })
+
+  function checkForVisibility() {
+    var headers = document.querySelectorAll(".visible");
+    headers.forEach(function(header) {
+      if (isElementInViewport(header)) {
+        header.classList.add("isVisible");
+      } 
+    });
+  }
+  
+  function isElementInViewport (el) {
+    var rect = el.getBoundingClientRect();
+  
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth) 
+    );
+  }
+  
+  if (window.addEventListener) {
+    addEventListener('DOMContentLoaded', checkForVisibility, false); 
+    addEventListener('load', checkForVisibility, false);
+    addEventListener('scroll', checkForVisibility, false);
+  }
